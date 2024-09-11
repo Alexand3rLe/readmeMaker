aa733b1f90b24d79399be74a2b52ccde724f3c1f
Alexander_L <alexander.w.leyva@gmail.com>
2024-09-10 21:32:05 -0500

    readme maker

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# readmeMaker
+# Read Me Maker
+  # Description
+  Generates a readme file through user inputs 
+  # Table of Contents
+  installation
+  ussage
+  license
+  contributing
+  tests
+  questions
+  # Installation
+  npm -i inquirer  
+  # Usage
+  to generate a readme.md
+  #License
+  MIT
+  # Contributing 
+  na 
+  # Tests 
+  n/a 
+  # Questions
+  xanleyva@gmail.com
--- index.js
+++ index.js
@@ -1,9 +1,83 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer
-  .prompt([
-    {
-      message: 'Press ENTER to continue...',
-      name: 'enterKey'
-    }
-  ]);
+const generateReadMe = ({ title, description, installation, usage, license, contributing, tests, questions
+ }) => 
+  `# ${title}
+  # Description
+  ${description} 
+  # Table of Contents
+  installation
+  ussage
+  license
+  contributing
+  tests
+  questions
+  # Installation
+  ${installation} 
+  # Usage
+  ${usage}
+  #License
+  ${license}
+  # Contributing 
+  ${contributing} 
+  # Tests 
+  ${tests} 
+  # Questions
+  ${questions}`
+
+const writeToReadMe = (file, data) => {
+  // console.log(file, data)
+  fs.writeFile(file, data, (error) => error ? console.error(error) : console.log('File made'));
+}
+
+
+inquirer.prompt([
+  {
+    name: "title",
+    type: "input",
+    message: "What is the title of your project?",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "give a brief description of your project"
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "give a brief description of how to install packages"
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "what is the usage of this app"
+  },
+  {
+    name: "license",
+    type: "rawlist",
+    choices: ["MIT", "GNU v3.0", "APACHE 2.0", "None"],
+    message: "what license should your prject have"
+  },
+  {
+    name: "contributing",
+    type: "input",
+    message: "contribution guidelines"
+  },
+  {
+    name: "tests",
+    type: "input",
+    message: "test instructions"
+  },
+  {
+    name: "questions",
+    type: "input",
+    message: "What is your email"
+  }
+
+])
+.then((res) => {
+  let readMe= generateReadMe(res)
+  console.log(readMe)
+  writeToReadMe('README.md', readMe )
+})
